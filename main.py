41d79d077952b9c713f4e6977af871e9bc27caeb
itsy467 <76798911+itsy467@users.noreply.github.com>
2021-06-22 12:56:05 +0900

    add to A+B

--- main.py
+++ main.py
@@ -1,110 +1,112 @@
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    basic.showLeds(`
+def on_button_pressed_a():
+    basic.clear_screen()
+    basic.show_leds("""
         . . # . .
         . # . # .
         . # . # .
         . . . . .
         . . . . .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . . # . .
         . # . # .
         . # . # .
         . # . # .
         . # . # .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . . # . .
         . # . # .
         . # # # .
         . # . # .
         . # . # .
-        `)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    basic.showLeds(`
+        """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.clear_screen()
+    basic.show_leds("""
         . . # . .
         . # . # .
         . # . # .
         . . . . .
         . . . . .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . . # . .
         . # . # .
         . # . # .
         . # . # .
         . # . # .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . . # . .
         . # . # .
         . # # # .
         . # . # .
         . # . # .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
-        . . . . .
+    basic.show_leds("""
         . . # . .
-        . # # # .
         . . # . .
-        . . . . .
-        `)
+        # # # # #
+        . . # . .
+        . . # . .
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . # . . .
         . # . . .
         . # . . .
         . # . . .
         . # . . .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . # # . .
         . # . . .
         . # # . .
         . # . . .
         . # # . .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . # # . .
         . # . # .
         . # # . .
         . # . # .
         . # # . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    basic.showLeds(`
+        """)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.clear_screen()
+    basic.show_leds("""
         . # . . .
         . # . . .
         . # . . .
         . # . . .
         . # . . .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . # # . .
         . # . . .
         . # # . .
         . # . . .
         . # # . .
-        `)
+        """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . # # . .
         . # . # .
         . # # . .
         . # . # .
         . # # . .
-        `)
-})
+        """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
