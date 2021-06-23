814456671d30fece80bce5da9e5f4eebe0a72626
itsy467 <76798911+itsy467@users.noreply.github.com>
2021-06-24 08:34:44 +0900

    常時実行されるコードとそれの制御を追加

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+def on_pin_pressed_p0():
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . # . .
+        """)
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
 def on_button_pressed_a():
     basic.clear_screen()
     basic.show_leds("""
@@ -48,6 +67,25 @@ def on_button_pressed_a():
         . # . # .
         """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_pin_pressed_p2():
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . # # # .
+        . . . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        """)
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
 
 def on_button_pressed_ab():
     basic.clear_screen()
@@ -204,3 +242,88 @@ def on_button_pressed_b():
         . # # . .
         """)
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_pin_pressed_p1():
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        """)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+def on_forever():
+    basic.show_leds("""
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . # # . .
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . # # .
+        . # # . .
+        # # . . .
+        # . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . # #
+        . . # # .
+        . # # . .
+        # # . . .
+        # . . . .
+        """)
+    basic.show_leds("""
+        . . . . #
+        . . . # #
+        . . # # .
+        . # # . .
+        # # . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # .
+        . # # . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        """)
+    basic.show_leds("""
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -7,7 +7,6 @@ input.onPinPressed(TouchPin.P0, function () {
         . # . . .
         . # . . .
         `)
-    basic.pause(100)
     basic.showLeds(`
         . . # . .
         . # . # .
@@ -257,67 +256,69 @@ input.onPinPressed(TouchPin.P1, function () {
         `)
 })
 basic.forever(function () {
-    basic.showLeds(`
-        # # . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # # . .
-        # # . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . # # .
-        . # # . .
-        # # . . .
-        # . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . # #
-        . . # # .
-        . # # . .
-        # # . . .
-        # . . . .
-        `)
-    basic.showLeds(`
-        . . . . #
-        . . . # #
-        . . # # .
-        . # # . .
-        # # . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # #
-        . . # # .
-        . # # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . # #
-        . . # # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . # #
-        `)
-    basic.showLeds(`
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . #
-        `)
+    if (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.A) || (input.pinIsPressed(TouchPin.P0) || input.pinIsPressed(TouchPin.P1) || input.pinIsPressed(TouchPin.P2)))) {
+        basic.showLeds(`
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # . .
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # # .
+            . # # . .
+            # # . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . # # .
+            . # # . .
+            # # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # .
+            . # # . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # #
+            . . # # .
+            . # # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . # #
+            . . # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . # #
+            `)
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
 })
